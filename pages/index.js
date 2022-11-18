1db10c86006dd32bdb00be4952466e5684f53f07
Marcus Lim <mj_lim0802@yahoo.com>
2022-11-17 20:28:34 -0600

    prettied up login page

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,55 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div class="bg-zinc-800 w-screen h-screen flex justify-center items-center">
-      <div id="card" class="bg-black rounded-xl p-4 w-1/9">
-        <div class="flex justify-center">
-          <h1 class="text-4xl font-bold text-white">Flyer Central</h1>
-        </div>
-
-        <div class="flex justify-center">
-          <img class="w-1/2 h-1/2"src="Flyer-Logo.PNG"></img>
-        </div>
-        
-        <div class="flex justify-center">
-          <a href="/login">
-            <button class="bg-red-900 hover:bg-red-800 text-white font-bold py-2 px-4 rounded">
-              Sign In with your Lewis E-Mail
-            </button>
-          </a>
+    <div class="flex columns-2">
+      <div id="image-section" class="bg-black">
+        <img class="h-full opacity-90" src="The-Encounter.jpg"></img>
+      </div>
+      <div
+        id="login-section"
+        class="flex justify-center items-center h-screen bg-black"
+      >
+        <div>
+          <div class="flex justify-center">
+            <img class="w-1/3" src="FLyer-Logo.png"></img>
+          </div>
+          <div class="flex justify-center mb-6">
+            <p class="font Calibri text-5xl text-white m-3">FLYER CENTRAL</p>
+          </div>
+          <div class="flex justify-center">
+            <div class="w-72">
+              <div class="flex justify-center">
+                <button
+                  class="bg-white hover:bg-gray-200 text-black w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Google
+                </button>
+              </div>
+              <div class="relative flex py-2 items-center">
+                <div class="flex-grow border-t border-gray-700"></div>
+                <span class="flex-shrink mx-4 text-white">or</span>
+                <div class="flex-grow border-t border-gray-700"></div>
+              </div>
+              <div class="flex justify-center">
+                <button
+                  class="bg-red-800 hover:bg-red-00 text-white w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Lewis
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center my-1">
+            <div class="w-64">
+              <div>
+                <p class="text-white text-xs">By signing up with us you agree to <a href="">the Terms of Service</a> and <a href="">Privacy Policy</a>, including <a href="">Cookie Use</a></p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    
-    
-    );
+  );
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -18,55 +18,110 @@ const login = () => {
       );
     } else {
       return (
-        <div class="bg-zinc-800 w-screen h-screen flex justify-center items-center">
-        <div id="card" class="bg-black rounded-xl p-4 w-1/23">
+        <div class="flex columns-2">
+      <div id="image-section" class="bg-black">
+        <img class="h-full opacity-90" src="The-Encounter.jpg"></img>
+      </div>
+      <div
+        id="login-section"
+        class="flex justify-center items-center h-screen bg-black"
+      >
+        <div>
           <div class="flex justify-center">
-            <h1 class="text-4xl font-bold text-white">Flyer Central</h1>
+            <img class="w-1/3" src="FLyer-Logo.png"></img>
           </div>
-
-          <div class="flex justify-center">
-            <img class="w-1/2 h-1/2" src="Flyer-Logo.PNG"></img>
+          <div class="flex justify-center mb-6">
+            <p class="font-mono text-5xl text-white m-3">FLYER CENTRAL</p>
           </div>
           <div class="flex justify-center">
-            <p class="text-white font-bold">You must sign-in with a Lewis Account!</p>
+            <div class="w-72">
+              <div class="flex justify-center">
+                <button
+                  class="bg-white hover:bg-gray-200 text-black w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Google
+                </button>
+              </div>
+              <div class="relative flex py-2 items-center">
+                <div class="flex-grow border-t border-gray-700"></div>
+                <span class="flex-shrink mx-4 text-white">or</span>
+                <div class="flex-grow border-t border-gray-700"></div>
+              </div>
+              <div class="flex justify-center">
+                <button
+                  class="bg-red-800 hover:bg-red-00 text-white w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Lewis
+                </button>
+              </div>
+            </div>
           </div>
-
-          <div class="flex justify-center">
-            <button
-              class="bg-red-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => signIn("google")}
-            >
-              Sign in with your Lewis Account
-            </button>
-            ;
+          <div class="flex justify-center my-1">
+            <div class="w-64">
+              <div>
+                <p class="text-white text-xs">By signing up with us you agree to <a href="">the Terms of Service</a> and <a href="">Privacy Policy</a>, including <a href="">Cookie Use</a></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
       );
     }
   } else {
     return (
-      <div class="bg-zinc-800 w-screen h-screen flex justify-center items-center">
-        <div id="card" class="bg-black rounded-xl p-4 w-1/2">
+      <div class="flex columns-2">
+      <div id="image-section" class="bg-black">
+        <img class="h-full opacity-90" src="The-Encounter.jpg"></img>
+      </div>
+      <div
+        id="login-section"
+        class="flex justify-center items-center h-screen bg-black"
+      >
+        <div>
           <div class="flex justify-center">
-            <h1 class="text-4xl font-bold text-white">Flyer Central</h1>
+            <img class="w-1/3" src="FLyer-Logo.png"></img>
           </div>
-
-          <div class="flex justify-center">
-            <img class="w-1/4 h-1/4" src="Flyer-Logo.PNG"></img>
+          <div class="flex justify-center mb-6">
+            <p class="font-mono text-5xl text-white m-3">FLYER CENTRAL</p>
           </div>
-
           <div class="flex justify-center">
-            <button
-              class="bg-red-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => signIn("google")}
-            >
-              Sign in with your Lewis Account
-            </button>
-            ;
+            <div class="w-72">
+              <div class="flex justify-center">
+                <button
+                  class="bg-white hover:bg-gray-200 text-black w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Google
+                </button>
+              </div>
+              <div class="relative flex py-2 items-center">
+                <div class="flex-grow border-t border-gray-700"></div>
+                <span class="flex-shrink mx-4 text-white">or</span>
+                <div class="flex-grow border-t border-gray-700"></div>
+              </div>
+              <div class="flex justify-center">
+                <button
+                  class="bg-red-800 hover:bg-red-00 text-white w-72 py-3 font-bold rounded-full"
+                  onClick={() => signIn("google")}
+                >
+                  Sign in with Lewis
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center my-1">
+            <div class="w-64">
+              <div>
+                <p class="text-white text-xs">By signing up with us you agree to <a href="">the Terms of Service</a> and <a href="">Privacy Policy</a>, including <a href="">Cookie Use</a></p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
     );
   }
 };
