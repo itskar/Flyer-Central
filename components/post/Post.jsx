9f463d7e03a83943d38ff996719ee82da262704f
Karthik Umashankar <KUmashankar@midtronics.com>
2022-11-27 17:56:49 -0600

    Fixed post responsiveness

--- components/post/Post.jsx
+++ components/post/Post.jsx
@@ -15,11 +15,11 @@ const Post = () => {
     return (
         <div className="mb-2 relative">
             <div className="flex flex-row">
-                <div className="my-5 ml-5 mr-2 h-14 w-14 rounded-full overflow-hidden">
+                <div className="my-5 ml-5 mr-2 h-12 w-12 xl:h-14 xl:w-14 rounded-full overflow-hidden">
                     <Image src={UserImg} alt="User"/>
                 </div>
                 <div className="py-5">
-                    <a href="" className="font-bold hover:underline">Flyer</a>
+                    <a href="" className="font-bold hover:underline ml-2">Flyer</a>
                 </div>
                 <div className="py-5 px-1">
                     <p className="text-[#6e767d] text-base">@FlyerCentral</p>
@@ -29,8 +29,8 @@ const Post = () => {
                 </div>
             </div>
             <div className="ml-[85px] relative">
-                <div className="w-[500px]">
-                    <p className="absolute top-[-45px] text-slate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                <div className="w-[400px]">
+                    <p className="absolute top-[-40px] xl:top-[-45px] xl:ml-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
                     nisi ut aliquip ex ea commodo consequat.</p>
