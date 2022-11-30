7477556fa85952d468fa22392d482c8b0a17f1f2
Karthik Umashankar <kumashankar@gmail.com>
2022-11-30 16:03:46 -0600

    Updated sidebar to use user data

--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import Logo from "../../public/static/Logo.png";
 import UserImg from "../../public/static/user-icon.png";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import Pages from "../Pages";
 
 import {
@@ -26,14 +27,14 @@ import {
   EllipsisHorizontalIcon as EllipsisHorizontalIconOutline,
 } from "@heroicons/react/24/outline";
 import SidebarLink from "./SidebarLink";
+import { data } from "autoprefixer";
 
 const Sidebar = ({activePage}) => {
-  const { data: session } = useSession();
-
+  const {data: session} = useSession();
   return (
-    <div className="hidden bg-[#0f0f0f] sm:flex flex-col items-center w-[96px] lg:ml-40 xl:items-start xl:w-[342px] xl:ml-64 p-2 fixed h-full">
-      <div className="flex flex-row items-center justify-center hover:cursor-pointer sm:w-14 sm:h-14 p-0 xl:ml-28 xl:w-20 xl:h-20 xl:mt-6 mt-4">
-        <Image src={Logo} alt="Lewis University" width={75} height={75} className="xl:ml-2" quality={100}/>
+    <div className="hidden sm:flex flex-col select-none items-center w-[96px] lg:ml-40 xl:items-start xl:w-[340px] xl:ml-64 p-2 fixed h-full">
+      <div className="flex flex-row items-center justify-center hover:cursor-pointer sm:w-14 sm:h-14 p-0 xl:ml-28 xl:w-20 xl:h-20 xl:mt-4">
+        <Image src={Logo} alt="Lewis University" width={75} height={75} className="xl:ml-1" quality={100}/>
         <h1 className="hidden xl:inline text-white text-2xl text-center font-Belleza leading-tight">
           FLYER CENTRAL
         </h1>
@@ -51,16 +52,12 @@ const Sidebar = ({activePage}) => {
         className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-16"
         onClick={signOut}
       >
-        <img
-          src={UserImg.src}
-          alt=""
-          className="h-10 w-10 rounded-full xl:mr-2.5"
-        />
+        <Image src={session?.user?.image} width ={50} height={50} className="rounded-full ml-1"/>
         <div className="hidden xl:inline leading-5">
-          <h4 className="font-bold">Flyer</h4>
-          <p className="text-[#6e767d]">@lewisu.edu</p>
+          <h4 className="font-bold ml-2">{session?.user?.name?.split(' ')[0]}</h4>
+          <p className="text-[#6e767d] ml-1.5">@{session?.user?.email?.split('@')[0]}</p>
         </div>
-        <EllipsisHorizontalIconOutline className="h-5 hidden text-textWhiteSecondary xl:inline ml-10" />
+        <EllipsisHorizontalIconOutline className="h-5 hidden text-textWhiteSecondary xl:inline ml-8" />
       </div>
     </div>
   );
