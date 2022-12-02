0d95b51ca708aee50c0c5cddcb2f24636431b664
Marcus Lim <mj_lim0802@yahoo.com>
2022-12-01 23:25:28 -0600

    added CreatePost component

--- components/feed/Feed.jsx
+++ components/feed/Feed.jsx
@@ -1,11 +1,13 @@
 import Logo from "../../public/static/Flyer-Logo.png";
 import Image from "next/image";
 import Post from "../post/Post"
+import CreatePost from "../createpost/CreatePost";
 
 const Feed = () => {
     return (
         <div className="text-textWhitePrimary flex-grow border-l-[1px] border-darkgray
         border-r max-w-[640px] sm:ml-[98px] lg:ml-[258px] xl:ml-[600px] divide-y-[1px] divide-darkgray">
+        <CreatePost/>
         <Post/>
         <Post/>
         <Post/>
