0d377b3253bd2d1bf436551dbf1950cb6fac7fba
Karthik Umashankar <kumashankar@gmail.com>
2022-12-06 15:53:42 -0600

    Fixed news margin

--- components/news/news.jsx
+++ components/news/news.jsx
@@ -3,7 +3,7 @@ import NewsButtons from './NewsButtons'
 
 const News = () => {
   return (
-    <div className='text-textWhitePrimary hidden lg:inline xl:w-[360px] py-10 space-y-5 ml-4'>
+    <div className='text-textWhitePrimary hidden lg:inline xl:w-[360px] py-10 space-y-5 ml-8'>
         <div  className='backdrop-brightness-150 rounded-xl h-[550px] xl:w-auto lg:w-auto '>
             <div className='flex flex-col'>
                 <div className='m-5'>
