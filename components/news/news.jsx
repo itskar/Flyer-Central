808fa29dc9b253288388841f6c59e6640b7fb0cb
Karthik Umashankar <kumashankar@gmail.com>
2022-12-06 15:47:01 -0600

    Fixed news component size and placement

--- components/news/news.jsx
+++ components/news/news.jsx
@@ -3,8 +3,8 @@ import NewsButtons from './NewsButtons'
 
 const News = () => {
   return (
-    <div className='text-textWhitePrimary hidden lg:inline xl:w-[360px] py-10 space-y-5 ml-10'>
-        <div  className='backdrop-brightness-150 rounded-xl h-[550px] xl:w-[23vw] lg:w-[23vw] '>
+    <div className='text-textWhitePrimary hidden lg:inline xl:w-[360px] py-10 space-y-5 ml-4'>
+        <div  className='backdrop-brightness-150 rounded-xl h-[550px] xl:w-auto lg:w-auto '>
             <div className='flex flex-col'>
                 <div className='m-5'>
                     <p className='font-semibold text-2xl'>What's happening</p>
