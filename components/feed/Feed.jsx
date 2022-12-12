c34bd56fc68c343ffcb1abfef09c800a60f84646
Karthik Umashankar <KUmashankar@midtronics.com>
2022-12-11 20:34:28 -0600

    Update feed header opacity

--- components/feed/Feed.jsx
+++ components/feed/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
         border-r max-w-[620px] sm:ml-[73px] xl:ml-[380px]"
     >
       
-      <div className="flex items-center sm:justify-between py-2 px-4 sticky top-0 z-50 backdrop-blur-xl bg-[#101116b0]">
+      <div className="flex items-center sm:justify-between py-2 px-4 sticky top-0 z-50 backdrop-blur-xl bg-[#10111683]">
         <h1 className="text-lg sm:text-xl font-semibold">Home</h1>
         <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
           <SparklesIcon className="h-5 text-white" />
